b80794f86c56616e0ba013dd145f5edb00bcc6c2
Kien Tran <kientran@kientran.com>
2010-04-27 17:06:15 -0500

    Add Blueprint CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
