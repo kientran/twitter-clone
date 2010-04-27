f97dd748e1c22c0bd50720187181179e8c411386
Kien Tran <kientran@kientran.com>
2010-04-27 17:44:23 -0500

    Finished layout and rotues

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.contact '/about', :controller => 'pages', :action => 'about'
+  map.contact '/help', :controller => 'pages', :action => 'help'
+  
+  map.signup '/signup', :controller => 'users', :action => 'new'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,7 +36,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+   map.root :controller => "pages", :action => 'home'
 
   # See how all your routes lay out with "rake routes"
 
