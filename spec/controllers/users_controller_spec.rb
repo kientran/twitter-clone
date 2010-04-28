74bfc0f6b3beb99ba889c2f4716acc379edacbb0
Kien Tran <kientran@kientran.com>
2010-04-28 00:19:47 -0500

    User model with passwords, and show user page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,34 @@
 require 'spec_helper'
 
 describe UsersController do
+  integrate_views
 
-  #Delete these examples and add some real ones
-  it "should use UsersController" do
-    controller.should be_an_instance_of(UsersController)
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+      User.stub!(:find, @user.id).and_return(@user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_tag("title", /#{@user.name}/)
+    end
+    
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_tag("h2", /#{@user.name}/)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_tag("h2>img", :class => "gravatar")
+    end
   end
-
 
   describe "GET 'new'" do
     it "should be successful" do
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name         "Michael Hartl"
+  user.email        "mhart1@example.com"
+  user.password     "foobar"
+  user.password_confirmation "foobar"
+end
